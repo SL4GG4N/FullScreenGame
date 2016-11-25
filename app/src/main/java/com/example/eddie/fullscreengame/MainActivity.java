694f1c56b1fab1c034eb464ebbd978f989b8b2ac
Eddie <eddiefor@kth.se>
2016-11-25 15:44:28 +0100

    Testar DisplayMetrics för att få ut längd och bredd

--- app/src/main/java/com/example/eddie/fullscreengame/MainActivity.java
+++ app/src/main/java/com/example/eddie/fullscreengame/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.eddie.fullscreengame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -13,15 +14,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        DisplayMetrics displaymetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
+        int height = displaymetrics.heightPixels;
+        int width = displaymetrics.widthPixels;
+
+
         Gameboard spelbrade = (Gameboard)findViewById(R.id.Gameboard);
 
-        toast(""+spelbrade.getMeasuredHeight());
-        toast(""+spelbrade.getMeasuredWidth());
-    }
-
-    private void toast(String text) {
-        Toast toast = Toast.makeText(this,text,Toast.LENGTH_SHORT);
-        toast.show();
+        toastText("" + height);
+        toastText("" + width);
     }
 
     private class SayHelloListener implements View.OnClickListener{
