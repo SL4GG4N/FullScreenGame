35e605843e61c4df851a44300e3417b632500c86
Zimeon <severalstars@gmail.com>
2016-11-25 16:51:14 +0100

    Spelplanen ritas nu ut.

--- app/src/main/java/com/example/eddie/fullscreengame/Gameboard.java
+++ app/src/main/java/com/example/eddie/fullscreengame/Gameboard.java
@@ -13,8 +13,6 @@ import android.view.View;
 
 public class Gameboard extends View {
 
-    Point[] points;
-
     @Override
     protected void onDraw(Canvas canvas) {
         Paint p = new Paint();
@@ -27,6 +25,59 @@ public class Gameboard extends View {
     private void nineMenMorrisBoard(Canvas canvas) {
         Paint paint = new Paint();
         paint.setColor(Color.BLACK);
+        paint.setStrokeWidth(4);
+        // TODO: 25/11/16 Automatisera 
+        drawLine(canvas,1,1,7,1,paint);
+        drawLine(canvas,7,1,7,7,paint);
+        drawLine(canvas,1,1,1,7,paint);
+        drawLine(canvas,1,7,7,7,paint);
+        drawLine(canvas,2,2,6,2,paint);
+        drawLine(canvas,2,6,6,6,paint);
+        drawLine(canvas,2,2,2,6,paint);
+        drawLine(canvas,6,2,6,6,paint);
+        drawLine(canvas,3,3,5,3,paint);
+        drawLine(canvas,5,3,5,5,paint);
+        drawLine(canvas,3,3,3,5,paint);
+        drawLine(canvas,3,5,5,5,paint);
+        drawLine(canvas,4,1,4,3,paint);
+        drawLine(canvas,4,7,4,5,paint);
+        drawLine(canvas,1,4,3,4,paint);
+        drawLine(canvas,5,4,7,4,paint);
+        float radius = Math.min(getWidth(),getHeight())*0.02f;
+        drawCircle(canvas,1,1,radius,paint);
+        drawCircle(canvas,4,1,radius,paint);
+        drawCircle(canvas,7,1,radius,paint);
+        drawCircle(canvas,2,2,radius,paint);
+        drawCircle(canvas,4,2,radius,paint);
+        drawCircle(canvas,6,2,radius,paint);
+        drawCircle(canvas,3,3,radius,paint);
+        drawCircle(canvas,4,3,radius,paint);
+        drawCircle(canvas,5,3,radius,paint);
+        drawCircle(canvas,1,4,radius,paint);
+        drawCircle(canvas,2,4,radius,paint);
+        drawCircle(canvas,3,4,radius,paint);
+        drawCircle(canvas,5,4,radius,paint);
+        drawCircle(canvas,6,4,radius,paint);
+        drawCircle(canvas,7,4,radius,paint);
+        drawCircle(canvas,3,5,radius,paint);
+        drawCircle(canvas,4,5,radius,paint);
+        drawCircle(canvas,5,5,radius,paint);
+        drawCircle(canvas,2,6,radius,paint);
+        drawCircle(canvas,4,6,radius,paint);
+        drawCircle(canvas,6,6,radius,paint);
+        drawCircle(canvas,1,7,radius,paint);
+        drawCircle(canvas,4,7,radius,paint);
+        drawCircle(canvas,7,7,radius,paint);
+    }
+
+    private void drawLine(Canvas c, int fromX, int fromY, int toX, int toY, Paint p) {
+        c.drawLine(abspos(fromX),abspos(fromY),abspos(toX),abspos(toY),p);
+    }
+    private void drawCircle(Canvas c, int x, int y, float rad, Paint p) {
+        c.drawCircle(abspos(x),abspos(y),rad, p);
+    }
+    private int abspos(int relpos) {
+        return Math.min(getWidth(),getHeight())*relpos*125/1000;
     }
 
     public Gameboard(Context context, AttributeSet attrs) {
@@ -39,9 +90,5 @@ public class Gameboard extends View {
 
     public Gameboard(Context context) {
         super(context);
-    }
-
-    private class Point {
-        int x,y;
     }
 }
