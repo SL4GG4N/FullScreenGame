c5ed79ec6b57e6d7e01a9b13d1ab6dc86e14a20a
Zimeon <severalstars@gmail.com>
2016-11-26 16:22:49 +0100

    MainActivity gör nu 18 pjäser; jag lyckades duplicera den första ImageViewn. Dock är det lite fult; jag skulle vilja göra allt programmatiskt... men det är en senare fråga.

--- app/src/main/java/com/example/eddie/fullscreengame/MainActivity.java
+++ app/src/main/java/com/example/eddie/fullscreengame/MainActivity.java
@@ -13,48 +13,48 @@ public class MainActivity extends AppCompatActivity {
 
     public static Gameboard spelbrade;
     public TextView infoPanel;
-    public ImageView sidView;
-    public ImageView sidView2;
+    public ImageView[] pawns;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Omforma br{det till en fyrkant
         DisplayMetrics displaymetrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
         int size = Math.min(displaymetrics.heightPixels, displaymetrics.widthPixels);
-
         spelbrade = (Gameboard)findViewById(R.id.Gameboard);
         spelbrade.getLayoutParams().height = size;
         spelbrade.getLayoutParams().width = size;
         spelbrade.invalidate();
         spelbrade.setOnTouchListener(new GameboardTouchListener());
+
+        // Initialisera textf{ltet.
         infoPanel = (TextView)findViewById(R.id.textView);
         infoPanel.setText("Här ska det stå nåt");
-        sidView = (ImageView)findViewById(R.id.imageView);
-        sidView.setX(0);
-        sidView.setY(0);
-        sidView.setMaxHeight(100);
-        sidView.setMaxWidth(100);
-        sidView.setAdjustViewBounds(true);
-        sidView2 = new ImageView(this);
-        sidView2.setImageResource(R.drawable.sidlogga);
-        sidView2.setLayoutParams(sidView.getLayoutParams());
-        sidView2.setX(10);
-        sidView2.setY(10);
-        sidView2.setMaxHeight(100);
-        sidView2.setMaxWidth(100);
-        sidView2.setAdjustViewBounds(true);
-        addContentView(sidView2,sidView2.getLayoutParams());
 
+        // Alla pj{ser.
+        pawns = new ImageView[18];
+        pawns[0]= (ImageView)findViewById(R.id.imageView);
+        for (int i=0; i<18; i++) {
+            if (pawns[i]==null) pawns[i]= new ImageView(this);
+            pawns[i].setImageResource(R.drawable.sidlogga);
+            pawns[i].setLayoutParams(pawns[0].getLayoutParams());
+            pawns[i].setX((i%9)*100);
+            pawns[i].setY(1300+(i/9)*100);
+            pawns[i].setMaxHeight(100);
+            pawns[i].setMaxWidth(100);
+            pawns[i].setAdjustViewBounds(true);
+            if (i>0) addContentView(pawns[i],pawns[i].getLayoutParams());
+        }
     }
 
     private class GameboardTouchListener implements View.OnTouchListener {
         @Override
         public boolean onTouch(View view, MotionEvent me) {
             if (me.getAction()==MotionEvent.ACTION_DOWN) {
-                int p = spelbrade.validateClick(sidView, (int)me.getX(),(int)me.getY());
+                int p = spelbrade.validateClick(pawns[0], (int)me.getX(),(int)me.getY());
                 infoPanel.setText("Du träffade "+ p);
                 if (p>=0) {
                     System.out.println("MainActivity såg en träff.");
